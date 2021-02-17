fecfbb00160c10d10bf60c225cd14fabcbb1078c
aspov <asp73rus@mail.ru>
2021-02-17 12:52:01 +0400

    add alt name field to user

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -11,6 +11,7 @@ class ModelCatalogProduct extends Model {
 			return array(
 				'product_id'       => $query->row['product_id'],
 				'name'             => $query->row['name'],
+				'alternative_name' => $query->row['alternative_name'],
 				'description'      => $query->row['description'],
 				'meta_title'       => $query->row['meta_title'],
 				'meta_description' => $query->row['meta_description'],
